5b6f2121dba501e9a5c3910fec5138daa77f5ce8
Syeda Nahal Mansoor <nahalsyeda@gmail.com>
2026-01-15 23:44:51 +0500

    Initial project files

---
+++ script.js
@@ -0,0 +1,18 @@
+// const text = "Inaya Enterprises"
+
+// let index = 0;
+// let mainPara = document.getElementById("main-p")
+// function typeEffect() {
+//     if (index === 0) {
+//         mainPara.innerHTML = ""
+//     }
+//   if (index < text.length) {
+//     mainPara.innerHTML += text.charAt(index);
+//     index++;
+//     setTimeout(typeEffect, 130); 
+//   }
+// }
+// window.onload = () => {
+//     index = 0;
+//     typeEffect();
+// };
